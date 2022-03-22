53c5b232a04e52d98196192bfbde035f33420cd3
DiegoRojo99 <diego99rojo@gmail.com>
2022-03-22 11:01:13 -0400

    No more submit button, answers selected already submits as answer

--- app/src/main/java/com/youngdred/friends_whodidit/GameActivity.java
+++ app/src/main/java/com/youngdred/friends_whodidit/GameActivity.java
@@ -17,7 +17,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     int answerSelected=-1;
     int currentQuestionIndex=0;
@@ -43,12 +43,50 @@ public class GameActivity extends AppCompatActivity {
         ImageButton answer1 = findViewById(R.id.img_btn_main_answer_1);
         ImageButton answer2 = findViewById(R.id.img_btn_main_answer_2);
         ImageButton answer3 = findViewById(R.id.img_btn_main_answer_3);
+
         ImageView cover = findViewById(R.id.imv_main_game_cover);
+        TextView pointsNumberText = findViewById(R.id.tv_game_points_number);
+        TextView levelNumberText = findViewById(R.id.tv_game_level_number);
+
         TextView questionText = findViewById(R.id.tv_main_question_text);
-        Button submit = findViewById(R.id.btn_main_submit);
+
+        updateQuestion();
+
+        answer0.setOnClickListener(this);
+        answer1.setOnClickListener(this);
+        answer2.setOnClickListener(this);
+        answer3.setOnClickListener(this);
+
+        endGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String totalA=(String) totalAnswers.getText();
+                String correctA=(String) correctAnswers.getText();
+                endGameMethod(correctA,totalA);
+            }
+        });
+    }
+
+    public void endGameMethod(String c, String t){
+        Intent summaryIntent= new Intent(GameActivity.this, FinishedGameSummary.class);
+        summaryIntent.putExtra("Correct Answers",c);
+        summaryIntent.putExtra("Total Answers",t);
+        startActivity(summaryIntent);
+    }
+
+    public String addAnswer(int totalAnswers){
+        return String.valueOf(totalAnswers+1);
+    }
+
+    public void updateQuestion(){
+
+        TextView questionText = findViewById(R.id.tv_main_question_text);
+        ImageButton answer0 = findViewById(R.id.img_btn_main_answer_0);
+        ImageButton answer1 = findViewById(R.id.img_btn_main_answer_1);
+        ImageButton answer2 = findViewById(R.id.img_btn_main_answer_2);
+        ImageButton answer3 = findViewById(R.id.img_btn_main_answer_3);
 
         questionText.setText(questionBank[currentQuestionIndex].questionText);
-
         switch (questionBank[currentQuestionIndex].answer0){
             case "Joey": answer0.setBackgroundResource(joey); break;
             case "Chandler": answer0.setBackgroundResource(chandler); break;
@@ -82,94 +120,56 @@ public class GameActivity extends AppCompatActivity {
             case "Phoebe": answer3.setBackgroundResource(phoebe); break;
         }
 
-        answer0.setOnClickListener(view -> answerSelected=0);
-        answer1.setOnClickListener(view -> answerSelected=1);
-        answer2.setOnClickListener(view -> answerSelected=2);
-        answer3.setOnClickListener(view -> answerSelected=3);
+    }
 
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                if(bankSize==currentQuestionIndex+1){
-                    if(answerSelected>=0&&answerSelected<4){
-                        if(questionBank[currentQuestionIndex].correctAnswer==answerSelected){
-                            correctAnswers.setText(addAnswer(Integer.parseInt((String) correctAnswers.getText())));
-                        }
-                        totalAnswers.setText(addAnswer(Integer.parseInt((String) totalAnswers.getText())));
-                        answerSelected=-1;
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.img_btn_main_answer_0){
+            answerSelected=0;
+        } else if(view.getId()==R.id.img_btn_main_answer_1){
+            answerSelected=1;
+        }else if(view.getId()==R.id.img_btn_main_answer_2){
+            answerSelected=2;
+        }else if(view.getId()==R.id.img_btn_main_answer_3){
+            answerSelected=3;
+        }
 
-                        endGameMethod((String) correctAnswers.getText(),(String) totalAnswers.getText());
+        if(view.getId()==R.id.img_btn_main_answer_0||
+                view.getId()==R.id.img_btn_main_answer_1||
+                view.getId()==R.id.img_btn_main_answer_2||
+                view.getId()==R.id.img_btn_main_answer_3){
+
+
+            TextView correctAnswers = findViewById(R.id.tv_main_correct_answers);
+            TextView totalAnswers = findViewById(R.id.tv_main_total_answers);
+            TextView questionText = findViewById(R.id.tv_main_question_text);
+
+
+            if(bankSize==currentQuestionIndex+1){
+                if(answerSelected>=0&&answerSelected<4){
+                    if(questionBank[currentQuestionIndex].correctAnswer==answerSelected){
+                        correctAnswers.setText(addAnswer(Integer.parseInt((String) correctAnswers.getText())));
                     }
-                }else{
-                    if(answerSelected>=0&&answerSelected<4){
-                        if(questionBank[currentQuestionIndex].correctAnswer==answerSelected){
-                            correctAnswers.setText(addAnswer(Integer.parseInt((String) correctAnswers.getText())));
-                        }
-                        totalAnswers.setText(addAnswer(Integer.parseInt((String) totalAnswers.getText())));
-                        answerSelected=-1;
-                        currentQuestionIndex++;
-                        questionText.setText(questionBank[currentQuestionIndex].questionText);
+                    totalAnswers.setText(addAnswer(Integer.parseInt((String) totalAnswers.getText())));
+                    answerSelected=-1;
+
+                    endGameMethod((String) correctAnswers.getText(),(String) totalAnswers.getText());
+                }
+            }else{
+                if(answerSelected>=0&&answerSelected<4){
+                    if(questionBank[currentQuestionIndex].correctAnswer==answerSelected){
+                        correctAnswers.setText(addAnswer(Integer.parseInt((String) correctAnswers.getText())));
                     }
+                    totalAnswers.setText(addAnswer(Integer.parseInt((String) totalAnswers.getText())));
+                    answerSelected=-1;
+                    currentQuestionIndex++;
+                    questionText.setText(questionBank[currentQuestionIndex].questionText);
                 }
-
-                questionText.setText(questionBank[currentQuestionIndex].questionText);
-                switch (questionBank[currentQuestionIndex].answer0){
-                    case "Joey": answer0.setBackgroundResource(joey); break;
-                    case "Chandler": answer0.setBackgroundResource(chandler); break;
-                    case "Monica": answer0.setBackgroundResource(monica); break;
-                    case "Ross": answer0.setBackgroundResource(ross); break;
-                    case "Rachel": answer0.setBackgroundResource(rachel); break;
-                    case "Phoebe": answer0.setBackgroundResource(phoebe); break;
-                }
-                switch (questionBank[currentQuestionIndex].answer1){
-                    case "Joey": answer1.setBackgroundResource(joey); break;
-                    case "Chandler": answer1.setBackgroundResource(chandler); break;
-                    case "Monica": answer1.setBackgroundResource(monica); break;
-                    case "Ross": answer1.setBackgroundResource(ross); break;
-                    case "Rachel": answer1.setBackgroundResource(rachel); break;
-                    case "Phoebe": answer1.setBackgroundResource(phoebe); break;
-                }
-                switch (questionBank[currentQuestionIndex].answer2){
-                    case "Joey": answer2.setBackgroundResource(joey); break;
-                    case "Chandler": answer2.setBackgroundResource(chandler); break;
-                    case "Monica": answer2.setBackgroundResource(monica); break;
-                    case "Ross": answer2.setBackgroundResource(ross); break;
-                    case "Rachel": answer2.setBackgroundResource(rachel); break;
-                    case "Phoebe": answer2.setBackgroundResource(phoebe); break;
-                }
-                switch (questionBank[currentQuestionIndex].answer3){
-                    case "Joey": answer3.setBackgroundResource(joey); break;
-                    case "Chandler": answer3.setBackgroundResource(chandler); break;
-                    case "Monica": answer3.setBackgroundResource(monica); break;
-                    case "Ross": answer3.setBackgroundResource(ross); break;
-                    case "Rachel": answer3.setBackgroundResource(rachel); break;
-                    case "Phoebe": answer3.setBackgroundResource(phoebe); break;
-                }
-
             }
-        });
 
-        endGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String totalA=(String) totalAnswers.getText();
-                String correctA=(String) correctAnswers.getText();
-                endGameMethod(correctA,totalA);
-            }
-        });
+           updateQuestion();
+
+        }
     }
-
-    public void endGameMethod(String c, String t){
-        Intent summaryIntent= new Intent(GameActivity.this, FinishedGameSummary.class);
-        summaryIntent.putExtra("Correct Answers",c);
-        summaryIntent.putExtra("Total Answers",t);
-        startActivity(summaryIntent);
-    }
-
-    public String addAnswer(int totalAnswers){
-        return String.valueOf(totalAnswers+1);
-    }
-
-
 }
